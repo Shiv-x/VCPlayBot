d7b0fb45b6965b0551146bb1ce550ad42ce7acb4
kucekmill <79855424+irwanramadhan@users.noreply.github.com>
2021-05-02 04:46:12 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -106,4 +106,4 @@ async def skip(_, message: Message):
 @errors
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    #await message.reply_text("✯𝗩𝗖𝗣𝗹𝗮𝘆𝗕𝗼𝘁✯=❇️ Admin cache refreshed!")
+    #await message.reply_text(*musicwan*=🌟"Admin cache refreshed!")
