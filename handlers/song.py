6333335fa2d1009ed7c79fba2e1bbd94efdff7be
kucekmill <79855424+irwanramadhan@users.noreply.github.com>
2021-05-03 05:06:40 +0700

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -382,7 +382,7 @@ async def ytmusic(client,message: Message):
     c_time = time.time()
     file_stark = f"{ytdl_data['id']}.mp4"
     capy = f"**Video Name ➠** `{thum}` \n**Requested For :** `{urlissed}` \n**Channel :** `{thums}` \n**Link :** `{mo}`"
-    await client.send_video(message.chat.id, video = open(file_stark, "rb"), duration = int(ytdl_data["duration"]), file_name = str(ytdl_data["title"]), thumb = sedlyf, caption = capy, supports_streaming = True , progress=progress, progress_args=(pablo, c_time, f''Peng {urlissed} Song From YouTube Music!`', file_stark))
+    await client.send_video(message.chat.id, video = open(file_stark, "rb"), duration = int(ytdl_data["duration"]), file_name = str(ytdl_data["title"]), thumb = sedlyf, caption = capy, supports_streaming = True , progress=progress, progress_args=(pablo, c_time, f''Peng {urlissed} Song From YouTube Music !`', file_stark))
     await pablo.delete()
     is_downloading = False
     for files in (sedlyf, file_stark):
