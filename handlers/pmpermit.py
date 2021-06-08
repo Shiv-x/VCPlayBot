824c9831c24501e1e7442e4afef3ea98d7dcdf73
ishwart466 <85565796+ishwart466@users.noreply.github.com>
2021-06-08 21:46:38 +0530

    Create pmpermit.py

---
+++ handlers/pmpermit.py
@@ -0,0 +1,9 @@
+from callsmusic.callsmusic import client as USER
+from pyrogram import filters
+from pyrogram.types import Chat, Message, User
+
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service @itz_zaynn .\n\n BOT IS MADE BY @itz_zayn")
+  return
