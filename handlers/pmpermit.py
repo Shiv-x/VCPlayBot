7be3effd853d62486e998bdb9050bafa1e1639ee
ishwart466 <85565796+ishwart466@users.noreply.github.com>
2021-06-08 21:14:21 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service @VCPlayBot .\n\n")
+  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service @itz_zaynn .\n\n BOT IS MADE BY @itz_zayn")
   return                        
