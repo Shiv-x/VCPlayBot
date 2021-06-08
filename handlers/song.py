d1b30f852ec6d00dc8afc5d4cc431b6073f91d4b
ishwart466 <85565796+ishwart466@users.noreply.github.com>
2021-06-08 19:26:33 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @VCPlayBot **'
+        rep = '**🎵 Uploaded by @itz_zayn **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
