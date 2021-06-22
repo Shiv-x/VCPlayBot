4750716ff5ebbd57b437ebf36539d2686bd0d42d
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-21 21:58:42 -0700

    Update pmpermit.py

--- VCPlayBot/modules/pmpermit.py
+++ VCPlayBot/modules/pmpermit.py
@@ -17,7 +17,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
+                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND YOUR GROUP INVITE LINK OR USERNAME HERE @AWESOMESUPPORT IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you need any help then join support group :- @AwesomeSupport\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
             )
             return
 
