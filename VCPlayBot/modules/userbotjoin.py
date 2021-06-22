5dc8c035262438729c7b040aaaa697eaf5fd5907
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-21 21:50:18 -0700

    Update userbotjoin.py

--- VCPlayBot/modules/userbotjoin.py
+++ VCPlayBot/modules/userbotjoin.py
@@ -21,7 +21,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "VCPlayBot"
 
     try:
         await USER.join_chat(invitelink)
@@ -34,7 +34,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @VCPlayRobot to your Group and try again</b>",
         )
         return
     await message.reply_text(
@@ -95,7 +95,7 @@ async def addcchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "VCPlayBot"
 
     try:
         await USER.join_chat(invitelink)
@@ -109,7 +109,7 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @VCPlayRobot to your Group and try again</b>",
         )
         return
     await message.reply_text(
