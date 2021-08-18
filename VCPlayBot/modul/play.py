e6b5e1b9204529bff3319cabadbbd01b61df917f
abinaya0101 <82209374+abinaya0101@users.noreply.github.com>
2021-08-18 11:01:54 +0800

    Update play.py
    fork update

--- VCPlayBot/modul/play.py
+++ VCPlayBot/modul/play.py
@@ -11,7 +11,6 @@ import wget
 from PIL import Image, ImageDraw, ImageFont
 from pyrogram import Client, filters
 from pyrogram.types import Voice
-from pyrogram.errors import UserAlreadyParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from Python_ARQ import ARQ
 from youtube_search import YoutubeSearch
