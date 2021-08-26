9e9b99fdf58df499eb65a294875531cc587d8c93
Avnish <83480217+Panther1999@users.noreply.github.com>
2021-08-26 21:38:18 +0530

    Update callsmusic.py

--- VCPlayBot/services/callsmusic/callsmusic.py
+++ VCPlayBot/services/callsmusic/callsmusic.py
@@ -16,4 +16,4 @@ def on_stream_end(chat_id: int) -> None:
         pytgcalls.leave_group_call(chat_id)
     else:
         pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
-run = pytgcalls.rum
+run() = pytgcalls.rum
