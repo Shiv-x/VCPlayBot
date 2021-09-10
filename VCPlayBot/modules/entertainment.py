d6f8cbd115b2946832de95900341ef40bbc3a333
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-10 13:42:20 +0800

    Create entertainment.py

---
+++ VCPlayBot/modules/entertainment.py
@@ -0,0 +1,53 @@
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME
+from helpers.filters import command
+
+@Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
+async def asupan(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
+async def wibu(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
+async def chika(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["truth", f"truth@{BOT_USERNAME}"]))
+async def truth(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/truth").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("something went wrong...")
+
+
+@Client.on_message(command(["dare", f"dare@{BOT_USERNAME}"]))
+async def dare(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/dare").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("something went wrong...")
